0349fa0745f25e3296cf4d1b4f4654da0a21e079
Robert Rynard <rmrynard@gmail.com>
2021-12-20 18:02:19 -0600

    add content from data.json

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 
 import { useEffect, useState } from "react";
 
+import profilePic from "./images/image-jeremy.png";
+
 function App() {
   const [data, setData] = useState([]);
 
@@ -17,12 +19,31 @@ function App() {
       .catch((err) => console.error("Error fetching data: ", err));
   }, []);
 
-  console.log(data);
-
   return (
     <div>
-      {data.map((info) => (
-        <div>{info.timeframes.daily.current}</div>
+      <div>
+        <div>
+          <img src={profilePic} alt="profile pic" />
+          <p>Report for</p>
+          <h2>Jeremy Robson</h2>
+        </div>
+        <div>
+          <button type="button">Daily</button>
+          <button type="button">Weekly</button>
+          <button type="button">Monthly</button>
+        </div>
+      </div>
+      {data.map((entry) => (
+        <div
+          className={"card " + entry.title.toLowerCase().replace(/\s/g, "-")}
+        >
+          <h2>{entry.title}</h2>
+          <button type="button">
+            <img src="./images/icon-ellipsis.svg" alt="..." />
+          </button>
+          <h3>{entry.timeframes.weekly.current}hrs</h3>
+          <p>Last Week - {entry.timeframes.weekly.previous}hrs</p>
+        </div>
       ))}
     </div>
   );
